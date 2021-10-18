ac7b36961dd5f5d17f4101f85e2546789f710816
Denner Rondinely <denner.rondinely@bizcap.com.br>
2021-10-18 19:37:47 -0300

    Congigurar pwa

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { register } from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-register();
+serviceWorkerRegistration.register();
 reportWebVitals();
